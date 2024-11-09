d64eb865b705a5a00a6409db41df219a1a29b002
Alistair Abao <abaolester@gmail.com>
2024-11-09 01:19:48 +0100

    created model for books and inital logic and layout  for adding books

--- client/src/components/Chips.jsx
+++ client/src/components/Chips.jsx
@@ -2,7 +2,7 @@ function Chips({ title, idprop, handleActive, active }) {
   return (
     /** @active state determines the style to be applied to each button depending on the state in the parent component (DashboardPage) */
     <section
-      className={`w-24 h-6 border-[0.5px] rounded-xl flex justify-center items-center text-[10px] uppercase font-semibold cursor-pointer md:text-lg md:w-32 md:hover:bg-indigo-50 md:hover:text-black md:active:bg-indigo-200 md:active:text-white ${
+      className={`w-24 h-6  border-[0.5px] rounded-xl flex justify-center items-center text-[10px] uppercase font-semibold cursor-pointer md:text-lg md:w-32 md:hover:bg-indigo-50 md:hover:text-black md:active:bg-indigo-200 md:active:text-white ${
         active === idprop
           ? "bg-indigo-300 text-white transition-colors"
           : "bg-transparent"
